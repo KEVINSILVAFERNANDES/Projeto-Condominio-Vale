8e9dd48f561709c7994175ad629345c1b271fe60
Caio Bahia <66456044+CaioBahia12@users.noreply.github.com>
2021-04-29 20:56:29 -0300

    Tela de Agendamento
    Commit correto da Tela de Agendamentos

---
+++ src/screens/Agendamento/data/users.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        id: 1,
+        local: 'Salão de festas',
+        data: '05/07/2021',
+        apartamento: '101',
+        
+        
+    },
+    {
+        id: 2,
+        local: 'Espaço gourmet',
+        data: '10/07/2021',
+        apartamento: '907',
+        
+    },
+   
+]
