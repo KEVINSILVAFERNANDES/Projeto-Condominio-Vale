35ed58fe82a121763187e775f58656e5b6d7dc60
KEVINSILVAFERNANDES <63075841+KEVINSILVAFERNANDES@users.noreply.github.com>
2021-06-05 16:39:34 -0300

    Atualizações

--- src/screens/AchadosPerdidos/views/UserForm.js
+++ src/screens/AchadosPerdidos/views/UserForm.js
@@ -1,11 +1,14 @@
 import React, { useContext, useState } from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Switch } from 'react-native'
 import { Button } from 'react-native-elements'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import UsersContext from '../context/UsersContext'
 
 export default ({route, navigation}) => {
     const [user, setUser] = useState(route.params ? route.params : {})
     const { dispatch } = useContext(UsersContext)
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
     return (
         <View style={style.form}>
             <Text>N° Apartamento:</Text>
@@ -32,7 +35,23 @@ export default ({route, navigation}) => {
                 value={user.telefone}
                 
             />
-            <Button
+            <Text>Ativar Notificações</Text>
+            <View style={styles.container}>
+                          
+                <Switch
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                
+                />
+                 
+                </View>
+                <Text></Text>
+
+
+            <Button style={{ color: '#A00'}}
                 title="Salvar"
                 onPress={() => {
                     dispatch({
@@ -42,10 +61,27 @@ export default ({route, navigation}) => {
                     navigation.goBack()
                 }}
             />
+            <TouchableOpacity
+                style={style.cancelar}
+                onPress={() => {
+                    navigation.goBack()
+                }}>
+                    <Text>Cancelar</Text>
+            </TouchableOpacity>
         </View>
     )
     
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      marginTop:10,
+      flexDirection: "row",
+
+    }
+  });
 
 const style = StyleSheet.create({
     form: {
@@ -56,6 +92,14 @@ const style = StyleSheet.create({
         borderColor: 'grey',
         borderWidth: 1,
         marginBottom: 10,
+        borderRadius: 20,
+        
+        
+    },
+    cancelar: {
+        marginTop:10,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
@@ -68,5 +112,6 @@ const style2 = StyleSheet.create({
         borderColor: 'grey',
         borderWidth: 1,
         marginBottom: 10,
+        borderRadius: 20,
     }
 })
