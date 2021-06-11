b57d6e4fbe8fd0c8deb882f278c2fd5c1f82d913
Guilherme Almeida <61845092+GuilhermeAlmeida12@users.noreply.github.com>
2021-06-10 21:56:40 -0300

    Atualizando o Sistema

--- src/screens/TarefaListaScreen/views/UserForm.js
+++ src/screens/TarefaListaScreen/views/UserForm.js
@@ -1,11 +1,15 @@
 import React, { useContext, useState } from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Picker, Switch } from 'react-native'
 import { Button } from 'react-native-elements'
 import UsersContext from '../context/UsersContext'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default ({route, navigation}) => {
     const [user, setUser] = useState(route.params ? route.params : {})
     const { dispatch } = useContext(UsersContext)
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    
     return (
         <View style={style.form}>
             <Text>Assunto:</Text>
@@ -24,6 +28,36 @@ export default ({route, navigation}) => {
                 value={user.aviso}
                 
             />
+
+            <Text>Aviso do Prédio:</Text>
+                        <View style={styless.container}>
+                        <Picker
+                            
+                            style={{ height: 50, width: 150 }}
+                            onValueChange={predio => setUser({...user, predio })}
+                            onValueChange={predio => setUser({...user, predio })}
+                            value={user.predio}
+                        >
+                            <Picker.Item label="Pádua" value= "Pádua" />
+                            <Picker.Item label="Basél" value= "Basél" />
+                        </Picker>
+                        </View>
+                        <Text></Text>
+
+                        <Text>Ativar Notificações</Text>
+            <View style={styles.container}>
+                          
+                <Switch
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                
+                />
+                 
+                </View>
+                <Text></Text>
             
             <Button
                 title="Salvar"
@@ -35,6 +69,13 @@ export default ({route, navigation}) => {
                     navigation.goBack()
                 }}
             />
+             <TouchableOpacity
+                style={style.cancelar}
+                onPress={() => {
+                    navigation.goBack()
+                }}>
+                    <Text>Cancelar</Text>
+            </TouchableOpacity>
         </View>
     )
     
@@ -49,6 +90,11 @@ const style = StyleSheet.create({
         borderColor: 'grey',
         borderWidth: 1,
         marginBottom: 10,
+    },
+    cancelar: {
+        marginTop:10,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
@@ -63,3 +109,22 @@ const style2 = StyleSheet.create({
         marginBottom: 10,
     }
 })
+
+const styless = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop:20,
+      alignItems: "center",
+      flexDirection: "row",
+    }
+  });
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      marginTop:10,
+      flexDirection: "row",
+
+    }
+  });
