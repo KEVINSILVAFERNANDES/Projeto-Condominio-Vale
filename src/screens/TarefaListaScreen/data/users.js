796fbff67b3988e8b7fb4c5617b619d9f83a06ac
Guilherme Almeida <61845092+GuilhermeAlmeida12@users.noreply.github.com>
2021-06-10 21:58:06 -0300

    Atualizando o Sistema

--- src/screens/TarefaListaScreen/data/users.js
+++ src/screens/TarefaListaScreen/data/users.js
@@ -1,12 +1,14 @@
 export default [
     {
         id: 1,
+        predio: 'Pádua',
         titulo: 'Manutenção dos elevadores!',
         aviso: 'Será realizada manutenção dos elevadores de serviço, nos dias: 05/06 e 09/06',
         
     },
     {
         id: 2,
+        predio: 'Basél',
         titulo: 'Limpeza da caixa de agua!',
         aviso: 'Será realizada limpeza na caixa de agua, será realizado um corte na agua no dia: 01/06 das 15:30 as 18:30',
         
