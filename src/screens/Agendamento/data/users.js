74ac3903a9f785b605d7edac014c5f1c90f6e5ec
Caio Bahia <66456044+CaioBahia12@users.noreply.github.com>
2021-06-10 21:51:49 -0300

    Atualização do sistema
    Melhorias gerais.

--- src/screens/Agendamento/data/users.js
+++ src/screens/Agendamento/data/users.js
@@ -3,6 +3,7 @@ export default [
         id: 1,
         local: 'Salão de festas',
         data: '05/07/2021',
+        predio: 'Pádua',
         apartamento: '101',
         
         
@@ -11,6 +12,7 @@ export default [
         id: 2,
         local: 'Espaço gourmet',
         data: '10/07/2021',
+        predio: 'Basél',
         apartamento: '907',
         
     },
